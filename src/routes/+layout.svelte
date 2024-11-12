567c9be289485df8dabe1e9c9cb770ea885a5302
Mi-Br <michailbredichin@gmail.com>
2024-11-12 09:07:42 +0100

    nav updates score

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,13 +10,19 @@ const {name, score} = $derived(getUserData())
         <ul class="menu menu-horizontal w-full">
                 <li><a href="/"><i aria-label="home" class="fa-solid fa-house"></i>Home</a></li>
                 <li><a href="/divisions">Divisions</a></li>
-            <div class="ml-auto">
-                <UserProfile name={name} score={score} />
-            </div>
+                 <div class="grid place-content-center">
+                    <div class="bg-base-100 rounded-full w-24 h-24 grid place-content-center" >
+                        <div>100</div>
+                    </div>
+                 </div>
+                <div class="ml-auto" style="outline: 1px solid pink;">
+                    <UserProfile name={name} score={score} />
+                </div>
         </ul>
     </nav>
-</div>  
-    <!-- 
+
+</div>
+    <!--
 {@render children()}
 
 
---
+++ src/routes/divisions/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+    let {children} = $props()
+</script>
+
+
+
+    <h1 class="title">Divisions</h1>
+    {@render children()}
+
+
+    <style>
+
+
+
+     .title {
+            color:var(--accent-clr);
+            font-size: 5rem;
+            text-align: center;
+        }
+
+    </style>
